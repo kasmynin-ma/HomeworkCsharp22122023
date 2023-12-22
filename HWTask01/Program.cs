da1a6d36bfea06645123119d17871ab9e34d660a
Maxim Kasmynin <kasmynin.m@gmail.com>
2023-12-22 11:06:01 +0300

    Task1 in the homework01Csharp22122023 is solved

---
+++ HWTask01/Program.cs
@@ -0,0 +1,23 @@
+﻿// Необходимо найти большее из двух чисел.
+// 1. Введем переменные первого и второго числа
+// 2. Введем условие нахождение большего числа
+// 3. Отобразим получившийся результата
+
+
+Console.Write("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber == secondNumber)
+{
+    Console.Write("Введенные числа равны" +" `" + firstNumber + "` ");
+}
+else
+    if (firstNumber > secondNumber)
+    {
+        Console.Write("Первое число" +" `" + firstNumber + "` " + "больше чем второе число" + " `" + secondNumber + "` ");
+    }
+    else
+    {
+        Console.Write("Первое число" +" `" + firstNumber + "` " + "меньше чем второе число" + " `" + secondNumber + "` ");
+    }   
