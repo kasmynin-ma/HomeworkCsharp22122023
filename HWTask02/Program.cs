23a1103e9e06ef420a282aacacca089e69aa2fcc
Maxim Kasmynin <kasmynin.m@gmail.com>
2023-12-25 13:33:24 +0300

    first verson HW Task02

---
+++ HWTask02/Program.cs
@@ -0,0 +1,35 @@
+﻿// Найти максимальное из трех чисел
+// 1. Задать на вход три числа a, b, c 
+// 1. Найти максимальное число из двух чисел a и b
+// 2. Сравнить полученное максимальное число с числом c
+// 3. вывести максимальное полученное число
+
+Console.Write("Введите первое число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("Максимально число: " + a);
+    }
+    else
+    {
+        Console.WriteLine("Максимально число: " + c);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.WriteLine("Максимально число: " + b);
+    }
+    else
+    {
+        Console.WriteLine("Максимально число: " + c);
+    }
+}
