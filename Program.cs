90f6dee52f40b34a8cdb066eff5a54e2338f5b04
Maxim Kasmynin <kasmynin.m@gmail.com>
2023-12-25 14:56:07 +0300

    task 03 comlited

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Введите свое решение ниже
+
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number%2)== 1) 
+{
+  Console.Write("Число" +" `" + number + "` " + "нечётное");
+}
+else 
+{
+  Console.Write("Число" +" `" + number + "` " + "чётное");
+}
